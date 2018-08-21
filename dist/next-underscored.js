77ff62b23805a6af99d8f8662b2ec5484bb93e19
afeiship <1290657123@qq.com>
2018-08-21 14:11:34 +0800

    issue: bugfix

--- dist/next-underscored.js
+++ dist/next-underscored.js
@@ -1,18 +1,19 @@
 (function () {
 
-  var global = global || this || self || window;
+  var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
-
 
   var UNDERLINE = '_';
   var dasherizeRE1 = /::/g,
       dasherizeRE2 = /([A-Z]+)([A-Z][a-z])/g,
       dasherizeRE3 = /([a-z\d])([A-Z])/g,
       dasherizeRE4 = /-/g,
+      dasherizeRE5 = /\//g,
       dasherizeREPLACER = '$1-$2';
 
   nx.underscored = function (inStr) {
     return inStr.replace(dasherizeRE1, '/')
+      .replace(dasherizeRE5, UNDERLINE)
       .replace(dasherizeRE2, dasherizeREPLACER)
       .replace(dasherizeRE3, dasherizeREPLACER)
       .replace(dasherizeRE4, UNDERLINE)
--- test/test.js
+++ test/test.js
@@ -4,14 +4,24 @@ require('../src/next-underscored');
 
 describe('next/underscored', function () {
 
-  it('nx.munderscoredix', function () {
+  it('nx.underscored font-size -> font_size', function () {
     var str1 = 'font-size';
-    var str2 = 'fontSize';
-    var str3 = 'FontSize';
+    assert.equal(nx.underscored(str1), 'font_size');
+  });
 
-    assert.equal( nx.underscored(str1), 'font_size');
-    assert.equal( nx.underscored(str2), 'font_size');
-    assert.equal( nx.underscored(str3), 'font_size');
+  it('nx.underscored fontSize -> font_size', function () {
+    var str2 = 'fontSize';
+    assert.equal(nx.underscored(str2), 'font_size');
+  });
+
+  it('nx.underscored FontSize -> font_size', function () {
+    var str3 = 'FontSize';
+    assert.equal(nx.underscored(str3), 'font_size');
+  });
+
+  it('nx.underscored /api/v1/test -> _api_v1_test', function () {
+    var str1 = '/api/v1/test';
+    assert.equal(nx.underscored(str1), '_api_v1_test');
   });
 
 });
